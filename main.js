9635a8e58acc2d8869317b8b35598e0bb1ca9575
root <sgarciap20@miumg.edu.gt>
2023-08-27 20:03:41 -0600

    se agrega el index y los archivos correspondientes a la peticion y a los estilos

---
+++ main.js
@@ -0,0 +1,23 @@
+const displayUsuarios = document.querySelector('.usuarios');
+const btnUsuarios = document.querySelector('#Cusuarios');
+
+btnUsuarios.addEventListener('click', getUsuarios);
+
+function getUsuarios () {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => response.json())
+    .then((data) => {
+        const insertuser = data.map(user =>
+            `<li>
+            id: ${user.id} 
+            <br> nombre: ${user.name} 
+            <br> username: ${user.username} 
+            <br> email" ${user.email} 
+            <br> direccion: ${user.address.street}
+            ${user.address.suite} ${user.address.city}
+            ${user.address.zipcode}
+            </li>`);
+        displayUsuarios.innerHTML = `<ul>  ${insertuser} </ul>`;
+    })
+    .catch((error) => console.log(error));
+}
